da71159d54c5fd53652a078301bb135af2e1f04e
Momtchil Momtchev <momtchil@momtchev.com>
2024-06-21 12:55:31 +0200

    switch the publishing script to ES6

--- scripts/publish-packages.mjs
+++ scripts/publish-packages.mjs
@@ -1,10 +1,15 @@
-const exec = require('util').promisify(require('child_process').exec)
-const { Octokit } = require('@octokit/core')
+import { promises as fs } from 'node:fs'
+import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import * as child_process from 'node:child_process'
+import { promisify } from 'node:util'
+import { Octokit } from '@octokit/core'
+const exec = promisify(child_process.exec)
 const octokit = new Octokit({ auth: process.env.NODE_PRE_GYP_GITHUB_TOKEN })
 const pkg = { repo: 'node-gdal-async', owner: 'mmomtchev' }
-const version = require('../package.json').version
+const version = JSON.parse(await fs.readFile(path.resolve(path.dirname(fileURLToPath(import.meta.url)),  '..', 'package.json'))).version
 const workflowPublishId = 7048427
-const workflowPublish = { ...pkg, workflow_id: workflowPublishId, ref: `v${version}` };
+const workflowPublish = { ...pkg, workflow_id: workflowPublishId, ref: `v${version}` }
 
 (async () => {
   const branch = (await exec('git branch --show-current')).stdout.trim()
